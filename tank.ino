6114592ab7e72c7914e696e55d5ba77e57b3d986
Maksym Bodnar <macsimbodnar@gmail.com>
2019-06-11 14:20:28 +0200

    Added bluetooth configuration

--- tank.ino
+++ tank.ino
@@ -1,3 +1,26 @@
+#include <SoftwareSerial.h>
+
+
+/***********************************************************************************
+ *                           DEFINES & CONSTANTS
+ **********************************************************************************/
+#define SETUP_BLUETOOTH_MODULE  1                // 0 disabled | 1 enabled
+#define BLUETOOTH_NAME          "nastytooth"
+#define BLUETOOTH_PWD           "0000"
+#define BLUETOOTH_BAUD          "4"             // id        baud rate             
+                                                // 1---------1200
+                                                // 2---------2400
+                                                // 3---------4800
+                                                // 4---------9600
+                                                // 5---------19200
+                                                // 6---------38400
+                                                // 7---------57600
+                                                // 8---------115200
+                                                // 9---------230400
+                                                // A---------460800
+                                                // B---------921600
+                                                // C---------1382400
+
 #define PIN_EN_1 3 
 #define PIN_IA_1 7
 #define PIN_IB_1 4
@@ -29,6 +52,9 @@
 #define PIN_TX   1
 
 
+/***********************************************************************************
+ *                              DATA STRUCTURES
+ **********************************************************************************/
 typedef enum { FORWARD, BACKWARD, STOP } direction_t;
 
 typedef struct
@@ -49,29 +75,23 @@ static const motor_data_t motors_data[] =
 };
 
 
-void setup() {
-    
-    for (unsigned int i = 0; i < sizeof(motors_data) / sizeof(motors_data[0]); i++) {
-        set_up_motor(motors_data[i]);
-        
-        set_motor(motors_data[i], FORWARD, 125);    
-    }
-
-    set_up_pwm();
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+/***********************************************************************************
+ *                                   GLOBALS
+ **********************************************************************************/
+static SoftwareSerial serial(PIN_RX, PIN_TX); // RX | TX
 
 
+
+/***********************************************************************************
+ *                                  FUNCTIONS
+ **********************************************************************************/
 void set_up_motor(motor_data_t motor)
 {
     pinMode(motor.pin_enable, OUTPUT);
     pinMode(motor.pin_in_a, OUTPUT);
     pinMode(motor.pin_in_b, OUTPUT);
 }
+
 
 void set_motor(motor_data_t motor, direction_t dir, unsigned char spd)
 {
@@ -100,7 +120,7 @@ void set_up_pwm()
     //---------------------------------------------- Set PWM frequency for D5 & D6 -------------------------------
 
     //TCCR0B = TCCR0B & B11111000 | B00000001;    // set timer 0 divisor to     1 for PWM frequency of 62500.00 Hz
-      TCCR0B = (TCCR0B & B11111000) | B00000010;    // set timer 0 divisor to     8 for PWM frequency of  7812.50 Hz
+    //TCCR0B = TCCR0B & B11111000 | B00000010;    // set timer 0 divisor to     8 for PWM frequency of  7812.50 Hz
     //TCCR0B = TCCR0B & B11111000 | B00000011;    // set timer 0 divisor to    64 for PWM frequency of   976.56 Hz (The DEFAULT)
     //TCCR0B = TCCR0B & B11111000 | B00000100;    // set timer 0 divisor to   256 for PWM frequency of   244.14 Hz
     //TCCR0B = TCCR0B & B11111000 | B00000101;    // set timer 0 divisor to  1024 for PWM frequency of    61.04 Hz
@@ -109,7 +129,7 @@ void set_up_pwm()
     //---------------------------------------------- Set PWM frequency for D9 & D10 ------------------------------
     
     //TCCR1B = TCCR1B & B11111000 | B00000001;    // set timer 1 divisor to     1 for PWM frequency of 31372.55 Hz
-      TCCR1B = (TCCR1B & B11111000) | B00000010;    // set timer 1 divisor to     8 for PWM frequency of  3921.16 Hz
+    //TCCR1B = TCCR1B & B11111000 | B00000010;    // set timer 1 divisor to     8 for PWM frequency of  3921.16 Hz
     //TCCR1B = TCCR1B & B11111000 | B00000011;    // set timer 1 divisor to    64 for PWM frequency of   490.20 Hz (The DEFAULT)
     //TCCR1B = TCCR1B & B11111000 | B00000100;    // set timer 1 divisor to   256 for PWM frequency of   122.55 Hz
     //TCCR1B = TCCR1B & B11111000 | B00000101;    // set timer 1 divisor to  1024 for PWM frequency of    30.64 Hz
@@ -117,10 +137,41 @@ void set_up_pwm()
     //---------------------------------------------- Set PWM frequency for D3 & D11 ------------------------------
     
     //TCCR2B = TCCR2B & B11111000 | B00000001;    // set timer 2 divisor to     1 for PWM frequency of 31372.55 Hz
-      TCCR2B = (TCCR2B & B11111000) | B00000010;    // set timer 2 divisor to     8 for PWM frequency of  3921.16 Hz
+    //TCCR2B = TCCR2B & B11111000 | B00000010;    // set timer 2 divisor to     8 for PWM frequency of  3921.16 Hz
     //TCCR2B = TCCR2B & B11111000 | B00000011;    // set timer 2 divisor to    32 for PWM frequency of   980.39 Hz
     //TCCR2B = TCCR2B & B11111000 | B00000100;    // set timer 2 divisor to    64 for PWM frequency of   490.20 Hz (The DEFAULT)
     //TCCR2B = TCCR2B & B11111000 | B00000101;    // set timer 2 divisor to   128 for PWM frequency of   245.10 Hz
     //TCCR2B = TCCR2B & B11111000 | B00000110;    // set timer 2 divisor to   256 for PWM frequency of   122.55 Hz
     //TCCR2B = TCCR2B & B11111000 | B00000111;    // set timer 2 divisor to  1024 for PWM frequency of    30.64 Hz
+}
+
+
+void set_up_bluetooth_module()
+{
+    serial.write("AT+NAME" BLUETOOTH_NAME);
+    serial.write("AT+PIN" BLUETOOTH_PWD);
+    serial.write("AT+BAUD" BLUETOOTH_BAUD);
+}
+
+
+/***********************************************************************************
+ *                                  LOGIC
+ **********************************************************************************/
+void setup() {
+    set_up_pwm();
+
+    for (unsigned int i = 0; i < sizeof(motors_data) / sizeof(motors_data[0]); i++) {
+        set_up_motor(motors_data[i]);
+        set_motor(motors_data[i], STOP, 0);    
+    }
+
+    serial.begin(9600);
+
+#if SETUP_BLUETOOTH_MODULE
+    set_up_bluetooth_module();
+#endif
+}
+
+void loop() {
+
 }
